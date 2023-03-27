d552475a29fdf9982a5f0ecbb91ea27746668c3b
SafaAl <127904342+SafaAL1@users.noreply.github.com>
2023-03-27 10:43:46 -0500

    Create Tile.java

---
+++ Tile.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tile;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Safa
+ */
+public class Tile {
+    
+    public BufferedImage image;
+    public boolean collision = false;
+}
